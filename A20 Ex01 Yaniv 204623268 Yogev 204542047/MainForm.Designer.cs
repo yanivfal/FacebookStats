19f215a83ef92f521b7a3d290912acbb21235d3b
falikyaniv@gmail.com <falikyaniv@gmail.com>
2019-11-19 18:42:58 +0200

    add Login

---
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/MainForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace A20_Ex01_Yaniv_204623268_Yogev_204542047
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(77, 33);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(159, 51);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
+
---
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using FacebookWrapper;
+using Facebook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+
+namespace A20_Ex01_Yaniv_204623268_Yogev_204542047
+{
+    public partial class MainForm : Form
+    {
+        User m_LoggedInUser;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
+        }
+
+        private void loginAndInit()
+        {
+            LoginResult result = FacebookWrapper.FacebookService.Login("2731122040258680", "email");
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+
+    }
+}
