b37eb0f89bc0397766139ea8af53a74af74c7ab2
falikyaniv@gmail.com <falikyaniv@gmail.com>
2019-11-25 16:53:51 +0200

    Add uploads statistics form

---
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/UploadsStats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A20_Ex01_Yaniv_204623268_Yogev_204542047
+{
+    public partial class UploadsStats : Form
+    {
+        public UploadsStats()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
