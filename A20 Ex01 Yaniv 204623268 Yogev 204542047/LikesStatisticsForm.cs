4e002bedbf804042321262e5b6b3be92266ef94f
Yogev Nissan <yogev.nissan@gmail.com>
2019-11-29 12:33:19 +0200

    want to sync with falik,

--- A20 Ex01 Yaniv 204623268 Yogev 204542047/LikesStatisticsForm.cs
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/LikesStatisticsForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace A20_Ex01_Yaniv_204623268_Yogev_204542047
 {
-    public partial class LinkesStatisticsForm : Form
+    public partial class LikesStatisticsForm : Form
     {
-        public LinkesStatisticsForm()
+        public LikesStatisticsForm()
         {
             InitializeComponent();
         }
