341967dab0c3feae9fd080b2383891f0532b99c8
falikyaniv@gmail.com <falikyaniv@gmail.com>
2019-11-30 13:47:30 +0200

    Add AccessTokenForTest to git ignore

--- A20 Ex01 Yaniv 204623268 Yogev 204542047/settings/AccessTokenForTest.cs
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/settings/AccessTokenForTest.cs
@@ -9,5 +9,6 @@ namespace A20_Ex01_Yaniv_204623268_Yogev_204542047.settings
     public static class AccessTokenForTest
     {
         public static string Access_Token = "EAAmz8OcMQHgBAGXfUC2lNN7aTYzkurCRoZA4hgNxHstJHmZAD7mQvaxlIOi3egFPJnsssVingLlokbEYpkPHgPhD3vXXoFR96nSZCJOgblroKlFEiaE0GMs73n20kh7qUMDOWK4uyprll0qCjNb1ROcreNXS6Iz0OWUmKnoxZA0dOJAZBd2njG7sRCvAf8qgJNiexXWPjRQZDZD";
+        //bla bla
     }
 }
