fa3fc7b58fe1e74946fb4bd44eaa46e1bf06e402
falikyaniv@gmail.com <falikyaniv@gmail.com>
2019-12-06 16:13:42 +0200

    Ready For real

--- A20 Ex01 Yaniv 204623268 Yogev 204542047/LoginForm.cs
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/LoginForm.cs
@@ -1,15 +1,5 @@
-﻿using FacebookWrapper;
-using Facebook;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
-using FacebookWrapper.ObjectModel;
 using A20_Ex01_Yaniv_204623268_Yogev_204542047.Logics;
 
 namespace A20_Ex01_Yaniv_204623268_Yogev_204542047
