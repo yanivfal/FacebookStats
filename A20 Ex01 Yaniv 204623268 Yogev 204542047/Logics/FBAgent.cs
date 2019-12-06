4a129b174f9b9d2bd67ab2d9d4ab76f820355c8a
Yogev Nissan <yogev.nissan@gmail.com>
2019-12-06 13:59:18 +0200

    Clean before submission (not finished)

--- A20 Ex01 Yaniv 204623268 Yogev 204542047/Logics/FBAgent.cs
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/Logics/FBAgent.cs
@@ -1,29 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using A20_Ex01_Yaniv_204623268_Yogev_204542047.settings;
-using Facebook;
 using FacebookWrapper;
 using FacebookWrapper.ObjectModel;
 
 namespace A20_Ex01_Yaniv_204623268_Yogev_204542047.Logics
 {
-    public static class FBAgent
+    internal static class FBAgent
     {
         internal static User LoggedInUser { get; set; }
 
-        public static bool LoginAndInit(out string o_AccessToken)
+        internal static bool LoginAndInit(out string o_AccessToken)
         {
             LoginResult loginResult;
             bool isLoginSucceeded = false;
             o_AccessToken = null;
+
             try
             {
                 loginResult = FacebookService.Login("2731122040258680", "public_profile", "user_birthday", "user_photos");
-                //loginResult = FacebookService.Connect(AccessTokenForTest.Access_Token);
+
                 if (!string.IsNullOrEmpty(loginResult.AccessToken))
                 {
                     isLoginSucceeded = true;
