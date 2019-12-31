f02e8b0aaa9318f559a8b7c3ca8313bb9555c153
Yogev Nissan <yogev.nissan@gmail.com>
2019-12-31 15:40:04 +0200

    Added PictureBoxProxy

---
+++ A20 Ex01 Yaniv 204623268 Yogev 204542047/Proxy/PictureBoxProxy.cs
@@ -0,0 +1,44 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A20_Ex01_Yaniv_204623268_Yogev_204542047.Proxy
+{
+
+    internal class PictureBoxProxy : PictureBox
+    {
+        private const int k_PhotoWidth = 400;
+
+        public PictureBoxProxy(Photo i_Photo)
+        {
+            Image = i_Photo.ImageNormal;
+            Height = calcHeightByratio(i_Photo);
+        }
+
+        private int calcHeightByratio(Photo i_Photo)
+        {
+            int imageHeight = k_PhotoWidth;
+            int imageWidth = k_PhotoWidth;
+
+            try
+            {
+                imageHeight = i_Photo.ImageNormal.Height;
+                imageWidth = i_Photo.ImageNormal.Width;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            float imageRatio = imageHeight / (float)imageWidth;
+            int height = (int)(k_PhotoWidth * imageRatio);
+
+            return height;
+        }
+    }
+}
